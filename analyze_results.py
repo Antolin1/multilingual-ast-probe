ec91acc56ed8725943d21a16c0ecc3518631cdd2
Antolin1 <joseantonio.hernandez6@um.es>
2022-11-08 09:39:49 +0100

    added go

--- analyze_results.py
+++ analyze_results.py
@@ -34,7 +34,7 @@ def read_results(args):
 
 def main(args):
     results = read_results(args)
-    for lang in ['python', 'java', 'ruby', 'javascript']:
+    for lang in ['python', 'java', 'ruby', 'javascript', 'go']:
         layer_vs_f1 = (
                 ggplot(results[(results['lang'] == lang)])
                 + aes(x="layer", y="f1", color='model')

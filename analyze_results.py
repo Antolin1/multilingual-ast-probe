06a5ff06654c88469999c1490163c888aa3b222e
Antolin1 <joseantonio.hernandez6@um.es>
2022-11-13 22:10:58 +0100

    analyze results

--- analyze_results.py
+++ analyze_results.py
@@ -28,12 +28,14 @@ def read_results(args):
                              'codeberta': 'CodeBERTa',
                              'codet5': 'CodeT5',
                              'graphcodebert': 'GraphCodeBERT',
-                             'roberta': 'RoBERTa'})
+                             'roberta': 'RoBERTa',
+                             'distilbert': 'DistilBERT',
+                             'bert': 'BERT',
+                             'distilroberta': 'DistilRoBERTa'})
     return df_renamed
 
 
-def main(args):
-    results = read_results(args)
+def plot_results_layer_vs_f1(results):
     for lang in ['python', 'java', 'ruby', 'javascript', 'go', 'c', 'csharp', 'php']:
         layer_vs_f1 = (
                 ggplot(results[(results['lang'] == lang)])
@@ -46,8 +48,20 @@ def main(args):
         layer_vs_f1.save(f"layer_vs_f1_{lang}.pdf", dpi=600)
 
 
+def best_layer_for_each_model(results):
+    print(results.groupby(['model', 'layer'])['f1'].mean())
+
+
+def main(args):
+    results = read_results(args)
+    plot_results_layer_vs_f1(results)
+    best_layer_for_each_model(results)
+    results.to_csv(args.out_csv_rq1, index=False)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script for analyzing the results')
     parser.add_argument('--run_dir', default='./runs', help='Path of the run logs')
+    parser.add_argument('--out_csv_rq1', default='rq1.csv', help='Csv name for the first rq1')
     args = parser.parse_args()
     main(args)

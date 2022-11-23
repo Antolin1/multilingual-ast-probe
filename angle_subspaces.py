3052724f729e1c448ab6b93fb623e99ab31d2d2b
hernanja <hernanja@ostende.iro.umontreal.ca>
2022-11-23 05:43:59 -0500

    bug

--- angle_subspaces.py
+++ angle_subspaces.py
@@ -12,48 +12,6 @@ from src.data import LANGUAGES_CSN
 from visualization_multilingual import load_vectors
 
 
-def main(args):
-    with open(args.out_best_layer_per_model_rq1) as json_file:
-        best_layer_per_model = json.load(json_file)
-    layer = None
-    for dict_model in best_layer_per_model:
-        if dict_model['model'] == ELEGANT_NAMES[args.model]:
-            layer = dict_model['layer']
-    subspaces = {}
-    for lang in LANGUAGES_CSN:
-        name_folder = '_'.join([args.model, lang, str(layer), '128'])
-        run_folder = os.path.join(args.run_dir, name_folder)
-        _, _, proj = load_vectors(run_folder)
-        subspaces[lang] = proj
-
-    table_sim_ang = PrettyTable()
-    table_sim_ang.field_names = ["----"] + list(LANGUAGES_CSN)
-
-    for x in LANGUAGES_CSN:
-        row_ang = [x]
-        angles = []
-        bleus = []
-        for y in LANGUAGES_CSN:
-            subspace_sim_ang = np.rad2deg(np.mean(subspace_angles(subspaces[x], subspaces[y])))
-            row_ang.append(round(subspace_sim_ang, 4))
-            if x != y:
-                angles.append(subspace_sim_ang)
-                bleus.append(DEVANBU_RESULTS[x][y])
-        table_sim_ang.add_row(row_ang)
-        print(f'Testing {x}, correlation: {spearmanr(bleus, angles)}')
-    print(table_sim_ang)
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Script for computing angle between subspaces')
-    parser.add_argument('--run_dir', default='./runs', help='Path of the run logs')
-    parser.add_argument('--model', help='Model name', choices=list(ELEGANT_NAMES.keys()), default='codebert')
-    parser.add_argument('--out_best_layer_per_model_rq1', default='best_layer_per_model.json',
-                        help='Json for the best layer per model')
-    args = parser.parse_args()
-    main(args)
-
-
 DEVANBU_RESULTS = {
     'ruby': {
         'ruby': 12.53,
@@ -104,3 +62,44 @@ DEVANBU_RESULTS = {
         'python': 18.25
     }
 }
+
+def main(args):
+    with open(args.out_best_layer_per_model_rq1) as json_file:
+        best_layer_per_model = json.load(json_file)
+    layer = None
+    for dict_model in best_layer_per_model:
+        if dict_model['model'] == ELEGANT_NAMES[args.model]:
+            layer = dict_model['layer']
+    subspaces = {}
+    for lang in LANGUAGES_CSN:
+        name_folder = '_'.join([args.model, lang, str(layer), '128'])
+        run_folder = os.path.join(args.run_dir, name_folder)
+        _, _, proj = load_vectors(run_folder)
+        subspaces[lang] = proj
+
+    table_sim_ang = PrettyTable()
+    table_sim_ang.field_names = ["----"] + list(LANGUAGES_CSN)
+
+    for x in LANGUAGES_CSN:
+        row_ang = [x]
+        angles = []
+        bleus = []
+        for y in LANGUAGES_CSN:
+            subspace_sim_ang = np.rad2deg(np.mean(subspace_angles(subspaces[x], subspaces[y])))
+            row_ang.append(round(subspace_sim_ang, 4))
+            if x != y:
+                angles.append(subspace_sim_ang)
+                bleus.append(DEVANBU_RESULTS[x][y])
+        table_sim_ang.add_row(row_ang)
+        print(f'Testing {x}, correlation: {spearmanr(bleus, angles)}')
+    print(table_sim_ang)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Script for computing angle between subspaces')
+    parser.add_argument('--run_dir', default='./runs', help='Path of the run logs')
+    parser.add_argument('--model', help='Model name', choices=list(ELEGANT_NAMES.keys()), default='codebert')
+    parser.add_argument('--out_best_layer_per_model_rq1', default='best_layer_per_model.json',
+                        help='Json for the best layer per model')
+    args = parser.parse_args()
+    main(args) 

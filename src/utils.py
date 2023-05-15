77f29e62ed8fd610e68389d097843394a270a34b
Antolin1 <joseantonio.hernandez6@um.es>
2023-05-15 23:24:11 +0200

    seed corrected

--- src/utils.py
+++ src/utils.py
@@ -6,13 +6,17 @@ import numpy as np
 import torch
 
 
-def set_seed(seed):
+`def seed_all(seed):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    os.environ['PYHTONHASHSEED'] = str(seed)
+
+    os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":16:8"
+    torch.use_deterministic_algorithms(True)
+
+    # Enable CUDNN deterministic mode
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 

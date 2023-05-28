bdb15e4d900cd9f51a46d309f9c8291284fcddd8
Antolin1 <joseantonio.herlop@gmail.comm>
2023-05-28 18:42:15 +0000

    bug

--- script_transfer.py
+++ script_transfer.py
@@ -6,8 +6,7 @@ from src.data import LANGUAGES
 def main():
     for source in LANGUAGES:
         for target in LANGUAGES:
-            os.system(f"CUDA_VISIBLE_DEVICES=1 python src/transfer.py "
-                      f"--do_train "
+            os.system(f"CUDA_VISIBLE_DEVICES=1 python src/run_transfer.py "
                       f"--source_model runs/codebert_{source}_5_128/pytorch_model.bin "
                       f"--target_model runs/codebert_{target}_5_128/pytorch_model.bin "
                       f"--source_lang {source} "
